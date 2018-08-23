4e7e3ece532e4e7f9eb24f60009e951af4d01bdd
Guillaume-Quinquet <guillaume.quinquet@hotmail.fr>
2018-08-23 09:54:46 +0200

    Caching of the files. Offline works

--- js/sw.js
+++ js/sw.js
@@ -1,23 +1,34 @@
 //Creating a static cache name
 const staticCache = 'restaurant-review-app-cache-v1';
+let cacheFiles = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/data/restaurants.json',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/css/styles.css',
+    '/img/1.jpg',
+    '/img/10.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+]
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCache)
     .then(cache => {
       console.log('Opening Cache');
-      return cache.addAll([
-        './',
-        './index.html',
-        './restaurant.html',
-        './data/restaurants.json',
-        './js/main.js',
-        './js/restaurant_info.js',
-        './js/dbhelper.js',
-        './css/styles.css'
-      ])
-      .catch( err => {
-        console.log(`The cache opening seems to have failed: ${err}`);
-      });
+      return cache.addAll(cacheFiles);
+    })
+    .catch( err => {
+      console.log(`The cache opening seems to have failed: ${err}`);
     })
   );
 });
